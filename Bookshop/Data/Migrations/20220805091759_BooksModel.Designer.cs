1de743692538c95557f1c72556003108e1384dbd
sara abbasi <saraabbasi7899@gmail.com>
2022-08-05 13:51:56 +0430

    BooksModel

--- Bookshop/Data/Migrations/20220805091759_BooksModel.Designer.cs
+++ Bookshop/Data/Migrations/20220805091759_BooksModel.Designer.cs
@@ -4,14 +4,16 @@ using Bookshop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bookshop.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220805091759_BooksModel")]
+    partial class BooksModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
